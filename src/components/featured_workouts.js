0408162378f6fc1cf29c62c75cf97856ddca8280
Rich Reneau <rich.reneau@gmail.com>
2018-10-09 06:06:21 -0400

    modified styling to thumbnail

--- src/components/featured_workouts.js
+++ src/components/featured_workouts.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import deadlift from '../images/deadlift.jpg'
+import deadlift from '../images/deadlift.jpg';
+import runner from '../images/runner.jpg';
+import pushUp from '../images/woman_pushup.jpg';
 
 function featuredWorkouts() {
+
     return (
         <section id="featured-workouts">
             <div className="container-fluid">
@@ -14,31 +17,28 @@ function featuredWorkouts() {
                         
                         <div class="col-sm-4">
                             <div className="thumbnail ">
-                                <img className="rtn-image-home" src={deadlift} alt="..." />
+                                <img src={deadlift} alt="..." />
                                 <div className="caption">
-                                    <h3>Thumbnail label</h3>
-                                    <p>...</p>
-                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" className="btn btn-default" role="button">Button</a></p>
+                                    <h3>Basic Strength Circuit</h3>
+                                    <p>Type: Strength</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-sm-4">
                             <div className="thumbnail">
-                                <img src="..." alt="..." />
+                                <img src={runner} alt="..." />
                                 <div className="caption">
-                                    <h3>Thumbnail label</h3>
-                                    <p>...</p>
-                                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" className="btn btn-default" role="button">Button</a></p>
+                                    <h3>Strength Training for Runners</h3>
+                                    <p>Type: Strength</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-sm-4">
                             <div className="thumbnail">
-                                <img src="..." alt="..." />
+                                <img src={pushUp} alt="..." />
                                 <div className="caption">
-                                    <h3>Thumbnail label</h3>
-                                    <p>...</p>
-                                    <p><a href="#" className="btn btn-primary" role="button">Button</a> <a href="#" className="btn btn-default" role="button">Button</a></p>
+                                    <h3>Body Weight HITT Workout</h3>
+                                    <p>Type: HITT</p>
                                 </div>
                             </div>
                         </div>
