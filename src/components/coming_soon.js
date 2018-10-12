b27248dd53e06d790027732a8a75a501f6c90ff3
Rich Reneau <rich.reneau@gmail.com>
2018-10-12 06:08:51 -0400

    added comments

--- src/components/coming_soon.js
+++ src/components/coming_soon.js
@@ -7,12 +7,12 @@ function ComingSoon() {
         <section id="coming-soon">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-lg-2"></div>
-                    <div className="col">
+                    <div className="col-md-2"></div>
+                    <div className="col-md-8">
                         <h2><span className="focal-item">Coming Soon</span></h2><br />
                         <h4>This is a demo site. &nbsp;It is my intention to continue building additional features and possibly deploy this product in the future. &nbsp;Future releases may include group tracking, social capabilities, workout cloning, interval tracking, exercise auto-detection, and more.  &nbsp; Please check back!  </h4>
                     </div>
-                    <div className="col-lg-2"></div>
+                    <div className="col-md-2"></div>
                 </div>
             </div>
         </section>
