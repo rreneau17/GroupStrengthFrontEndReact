5fae25b16ec70010ebc7e7c5bdef270f0279abaf
Rich Reneau <rich.reneau@gmail.com>
2018-10-12 18:34:45 -0400

    refactored carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -3,53 +3,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Carousel } from 'react-bootstrap';
-import slide1 from '../images/strength_training_pic.jpg';
-import slide2 from '../images/handshake.jpg';
-import slide3 from '../images/fitness_smiling_group.jpg';
-
 
 class CarouselGS extends Component {
 
+    renderSlide(picPath) {
+        return (
+            <Carousel.Item>
+                <img src={require(`../images/${picPath}`)} />
+                <Carousel.Caption>
+                    <h1>EASY TRACKING FOR STRENGTH<br />TRAINING, HIIT, AND POST REHAB</h1>
+                    <p>Use the power of a custom app developed on the Fitbit <br />platform to track and analyze your training sessions.</p>
+                    <a id="learn-more" href="#"><br />
+                    <i className="fa fa-arrow-circle-down" style={{fontSize: '3.5em', paddingBottom: '3px'}}></i><br />See More
+                    </a>
+                    <br /><br />
+                </Carousel.Caption>
+            </Carousel.Item>
+        )
+    }
+    
     render() {
-        // for some reason unable to map prop values to Carousel images, will research further and refactor
+        
         if (this.props.landing) {
             return (
                 <Carousel
-                    interval={5000}
+                    interval={6000}
                     controls={false}>
-                    <Carousel.Item>
-                        <img src={slide1} />
-                        <Carousel.Caption>
-                            <h1>EASY TRACKING FOR STRENGTH<br />TRAINING, HIIT, AND POST REHAB</h1>
-                            <p>Use the power of a custom app developed on the Fitbit <br />platform to track and analyze your training sessions.</p>
-                            <a id="learn-more" href="#"><br />
-                            <i className="fa fa-arrow-circle-down" style={{fontSize: '3.5em', paddingBottom: '3px'}}></i><br />See More
-                            </a>
-                            <br /><br />
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={slide2} />
-                        <Carousel.Caption>
-                            <h1>EASY TRACKING FOR STRENGTH<br />TRAINING, HIIT, AND POST REHAB</h1>
-                            <p>Use the power of a custom app developed on the Fitbit <br />platform to track and analyze your training sessions.</p>
-                            <a id="learn-more" href="#"><br />
-                            <i className="fa fa-arrow-circle-down" style={{fontSize: '3.5em', paddingBottom: '3px'}}></i><br />See More
-                            </a>
-                            <br /><br />
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={slide3} />
-                        <Carousel.Caption>
-                            <h1>EASY TRACKING FOR STRENGTH<br />TRAINING, HIIT, AND POST REHAB</h1>
-                            <p>Use the power of a custom app developed on the Fitbit <br />platform to track and analyze your training sessions.</p>
-                            <a id="learn-more" href="#"><br />
-                            <i className="fa fa-arrow-circle-down" style={{fontSize: '3.5em', paddingBottom: '3px'}}></i><br />See More
-                            </a>
-                            <br /><br />
-                        </Carousel.Caption>
-                    </Carousel.Item>
+                    {this.props.landing.map(this.renderSlide)}
                 </Carousel>
             )
         } else {
--- src/components/featured_workouts.js
+++ src/components/featured_workouts.js
@@ -9,47 +9,35 @@ function FeaturedWorkouts() {
 
     return (
         <section id="featured-workouts">
-            <div className="container-fluid">
-            <div className="row">
-                <div className="col-xl-2"></div>
-                <div className="col">
-                    <h2>Check out these featured workouts on <span className="focal-item">Group Strength</span></h2>
-                    <div className="col-lg-2"></div>
-                    <div className="col-lg-8">
-                        
-                        <div className="col-sm-4">
-                            <div className="thumbnail ">
-                                <img src={deadlift} alt="..." />
-                                <div className="caption">
-                                    <h3>Basic Strength Circuit</h3>
-                                    <p>Type: Strength</p>
-                                </div>
-                            </div>
+            <div className="container">
+                <h2>Check out these featured workouts on <span className="focal-item">Group Strength</span></h2>  
+                <div className="col-sm-4">
+                    <div className="thumbnail ">
+                        <img src={deadlift} alt="..." />
+                        <div className="caption">
+                            <h3>Basic Strength Circuit</h3>
+                            <p>Type: Strength</p>
                         </div>
-                        <div className="col-sm-4">
-                            <div className="thumbnail">
-                                <img src={runner} alt="..." />
-                                <div className="caption">
-                                    <h3>Strength Training for Runners</h3>
-                                    <p>Type: Strength</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-4">
-                            <div className="thumbnail">
-                                <img src={pushUp} alt="..." />
-                                <div className="caption">
-                                    <h3>Body Weight HITT Workout</h3>
-                                    <p>Type: HITT</p>
-                                </div>
-                            </div>
-                        </div>
-                        
                     </div>
-                    <div className="col-lg-2"></div>
                 </div>
-                <div className="col-xl-2"></div>
-            </div>  
+                <div className="col-sm-4">
+                    <div className="thumbnail">
+                        <img src={runner} alt="..." />
+                        <div className="caption">
+                            <h3>Strength Training for Runners</h3>
+                            <p>Type: Strength</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <div className="thumbnail">
+                        <img src={pushUp} alt="..." />
+                        <div className="caption">
+                            <h3>Body Weight HITT Workout</h3>
+                            <p>Type: HITT</p>
+                        </div>
+                    </div>
+                </div>  
             </div>     
         </section>
     );
